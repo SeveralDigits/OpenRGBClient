b693ca28ad3b79afb0fac521b2cca10b7c8e9967
SeveralDigits <xanderebs@gmail.com>
2026-02-06 12:53:47 +0100

    Add support for parsing command-line arguments for device, color and effects

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import openrgb
 from openrgb.utils import RGBColor, DeviceType, ZoneType
 import threading
+import argparse
 import signal
 import sys
 
@@ -112,9 +113,27 @@ class OpenRGBClient:
             print("No device selected. Use selectDeviceByName() to select a device first.")
 
 if __name__ == "__main__":
-    client = OpenRGBClient("localhost", 6742)
+    parser = argparse.ArgumentParser(description="OpenRGB Client")
+    parser.add_argument("--address", default="localhost", help="OpenRGB server address")
+    parser.add_argument("--port", type=int, default=6742, help="OpenRGB server port")
+    parser.add_argument("--device", help="Device name to select")
+    parser.add_argument("--effect", help="Effect to run")
+    parser.add_argument("--color", nargs=3, type=int, metavar=("R", "G", "B"), help="Color to set (RGB values 0-255)")
+    
+    args = parser.parse_args()
+    
+    if not args.effect and not args.color:
+        parser.error("Either --effect or --color must be specified")
+    elif args.effect and args.color:
+        parser.error("Cannot specify both --effect and --color")
+    elif args.device is None:
+        parser.error("--device is required")
 
-    def signal_handler(sig, frame):
+    args = parser.parse_args()
+
+    client = OpenRGBClient(args.address, args.port)
+
+    def signal_handler(_sig, _frame):
         print("Shutting down...")
         client.shutdown_event.set()
         if client.effect_thread and client.effect_thread.is_alive():
@@ -125,11 +144,14 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    client.selectDeviceByName("B550 GAMING X V2")
-    client.setEffect("rainbow-cycle")
-
-    try:
-        while True:
-            client.shutdown_event.wait(1)
-    except KeyboardInterrupt:
-        signal_handler(None, None)
+    client.selectDeviceByName(args.device)
+    if args.effect:
+        client.setEffect(args.effect)
+        try:
+            while True:
+                client.shutdown_event.wait(1)
+        except KeyboardInterrupt:
+            signal_handler(None, None)
+    elif args.color:
+        color = RGBColor(args.color[0], args.color[1], args.color[2])
+        client.setColor(color)
